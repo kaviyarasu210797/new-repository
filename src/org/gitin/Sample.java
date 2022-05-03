481c3da9c607b77c200fdb7badb4cf58e5d6dd64
kaviyarasu210797 <kaviyarasu210797>
2022-05-03 20:31:41 +0530

    add some codes

--- src/org/gitin/Sample.java
+++ src/org/gitin/Sample.java
@@ -1,9 +1,12 @@
 package org.gitin;
 
 public class Sample {
+	
+	static String s="hi all";
 	public static void main(String[] args) {
 		
 		System.out.println("hi welcome to global repository");
+		System.out.println(s);
 	}
 
 }
